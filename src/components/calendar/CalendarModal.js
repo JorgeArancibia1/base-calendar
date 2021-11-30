e5ad34e8674b9f43ee2de9eee389abb981fed409
Jorge <jarancibia@waypoint.com>
2021-11-30 15:41:22 -0300

    Hasta aqui funcionando la validacion de fecha

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -1,7 +1,8 @@
 import Modal from "react-modal";
 import DateTimePicker from "react-datetime-picker";
 import moment from "moment";
-// import { useState } from "react";
+import { useState } from "react";
+import Swal from "sweetalert2";
 
 const customStyles = {
 	content: {
@@ -16,16 +17,63 @@ const customStyles = {
 
 Modal.setAppElement("#root");
 
-const startDate = moment().minutes(0).seconds(0).add(1, "hours"); // 3:00:00
+const now = moment().minutes(0).seconds(0).add(1, "hours"); // 3:00:00
+const nowPlus1 = now.clone().add(1, "hours");
 
 export const CalendarModal = () => {
 	// const [isOpen, setIsOpen] = useState(true);
+	const [dateStart, setDateStart] = useState(now.toDate());
+	const [dateEnd, setDateEnd] = useState(nowPlus1.toDate());
+	const [titleValid, setTitleValid] = useState(true);
+	const [formValues, setFormValues] = useState({
+		title: "Evento",
+		notes: "",
+		start: now.toDate(),
+		end: nowPlus1.toDate(),
+	});
+
+	const { notes, title, start, end } = formValues;
 
 	const closeModal = () => {
 		// setIsOpen(false);
 	};
 
-	const handleStartDateChange = () => {};
+	const handleStartDateChange = (e) => {
+		setDateStart(e);
+		setFormValues({
+			...formValues,
+			start: e,
+		});
+	};
+
+	const handleEndDateChange = (e) => {
+		setDateEnd(e);
+		setFormValues({
+			...formValues,
+			end: e,
+		});
+	};
+
+	const handleInputChange = ({ target }) => {
+		setFormValues({
+			...formValues,
+			[target.name]: target.value,
+		});
+	};
+
+	const HandleSumbitForm = (e) => {
+		e.preventDefault();
+
+		const momentStart = moment(start);
+		const momentEnd = moment(end);
+
+		if (momentStart.isSameOrAfter(momentEnd)) {
+			// Validacion para que la fecha fin sea siempre mayor a la de inicio
+			Swal.fire("error", "La fecha de fin debe ser mayor a la fecha de inicio");
+		}
+
+		if (title.trim().length < 2) return setTitleValid(false);
+	};
 
 	return (
 		<Modal
@@ -39,18 +87,25 @@ export const CalendarModal = () => {
 		>
 			<h1> Nuevo evento </h1>
 			<hr />
-			<form className="container">
+
+			<form className="container" onSubmit={HandleSumbitForm}>
 				<div className="form-group">
 					<label>Fecha y hora inicio</label>
 					<DateTimePicker
 						onChange={handleStartDateChange}
-						value={startDate.toDate()}
+						value={dateStart}
+						className="form-control"
 					/>
 				</div>
 
 				<div className="form-group">
 					<label>Fecha y hora fin</label>
-					<input className="form-control" placeholder="Fecha inicio" />
+					<DateTimePicker
+						onChange={handleEndDateChange}
+						value={dateEnd}
+						minDate={dateStart}
+						className="form-control"
+					/>
 				</div>
 
 				<hr />
@@ -58,10 +113,12 @@ export const CalendarModal = () => {
 					<label>Titulo y notas</label>
 					<input
 						type="text"
-						className="form-control"
+						className={`form-control ${!titleValid && "is-invalid"}`}
 						placeholder="Título del evento"
 						name="title"
 						autoComplete="off"
+						value={title}
+						onChange={handleInputChange}
 					/>
 					<small id="emailHelp" className="form-text text-muted">
 						Una descripción corta
@@ -75,6 +132,8 @@ export const CalendarModal = () => {
 						placeholder="Notas"
 						rows="5"
 						name="notes"
+						value={notes}
+						onChange={handleInputChange}
 					></textarea>
 					<small id="emailHelp" className="form-text text-muted">
 						Información adicional

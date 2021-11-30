287065f2e417eeaf4f55a67e02cacc30d8ddf697
Jorge <jarancibia@waypoint.com>
2021-11-30 15:48:34 -0300

    Redux implementado, faltan los actions

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-	Redirect,
-} from "react-router-dom";
-import { LoginScreen } from "./components/auth/LoginScreen";
-import { CalendarScreen } from "./components/calendar/CalendarScreen";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store/store";
+import { AppRouter } from "./routers/AppRouter";
 
 const App = () => {
 	return (
 		<Provider store={store}>
-			<Router>
-				<Switch>
-					<Route exact path="/loginScreen">
-						<LoginScreen />
-					</Route>
-					<Route path="/">
-						<CalendarScreen />
-					</Route>
-					<Redirect to="/" />
-				</Switch>
-			</Router>
+			<AppRouter />
 		</Provider>
 	);
 };
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,24 @@
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from "react-router-dom";
+import { LoginScreen } from "../components/auth/LoginScreen";
+import { CalendarScreen } from "../components/calendar/CalendarScreen";
+
 export const AppRouter = () => {
 	return (
-		<div>
-			<h1>AppRouter</h1>
-		</div>
+		<Router>
+			<Switch>
+				<Route exact path="/loginScreen">
+					<LoginScreen />
+				</Route>
+				<Route path="/">
+					<CalendarScreen />
+				</Route>
+				<Redirect to="/" />
+			</Switch>
+		</Router>
 	);
 };

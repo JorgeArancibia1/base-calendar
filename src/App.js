1683f81c025a377f48e681249cc511db87788e1e
Jorge <jarancibia@waypoint.com>
2021-11-30 15:43:32 -0300

    Provider de redux funcionando

--- src/App.js
+++ src/App.js
@@ -2,26 +2,29 @@ import {
 	BrowserRouter as Router,
 	Switch,
 	Route,
-	// Link,
 	Redirect,
 } from "react-router-dom";
 import { LoginScreen } from "./components/auth/LoginScreen";
 import { CalendarScreen } from "./components/calendar/CalendarScreen";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import { store } from "./redux/store/store";
 
 const App = () => {
 	return (
-		<Router>
-			<Switch>
-				<Route exact path="/loginScreen">
-					<LoginScreen />
-				</Route>
-				<Route path="/">
-					<CalendarScreen />
-				</Route>
-				<Redirect to="/" />
-			</Switch>
-		</Router>
+		<Provider store={store}>
+			<Router>
+				<Switch>
+					<Route exact path="/loginScreen">
+						<LoginScreen />
+					</Route>
+					<Route path="/">
+						<CalendarScreen />
+					</Route>
+					<Redirect to="/" />
+				</Switch>
+			</Router>
+		</Provider>
 	);
 };
 
